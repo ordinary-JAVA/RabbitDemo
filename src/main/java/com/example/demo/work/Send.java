22adb7eabde961bc826df8bcdaa0a1e2ab7a9909
lizongren <lizongren@ebupt.com>
2019-10-30 16:50:10 +0800

    初始版本

---
+++ src/main/java/com/example/demo/work/Send.java
@@ -0,0 +1,31 @@
+package com.example.demo.work;
+
+import com.example.demo.ConnectionUtil;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+public class Send {
+
+    private final static String QUEUE_NAME = "test_queue_work";
+
+    public static void main(String[] argv) throws Exception {
+        // 获取到连接以及mq通道
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+
+        // 声明队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        for (int i = 0; i < 300000; i++) {
+            // 消息内容
+            String message = "" + i;
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+
+            //Thread.sleep( 1);
+        }
+
+        channel.close();
+        connection.close();
+    }
+}
